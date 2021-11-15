73558cf2d379bdfef383e0b45014f78ed7ca149e
Guilherme Silva <guilherme.dna06@gmail.com>
2021-11-15 17:40:06 -0300

    teste

--- test/models/funcionario_test.rb
+++ test/models/funcionario_test.rb
@@ -2,18 +2,18 @@ require "test_helper"
 
 class FuncionarioTest < ActiveSupport::TestCase
   test "criar funcionario valido" do
-    funcionario = Funcionario.new(nome: "junior", email: "zezejunior@email.com", senha: "S&nha123", senha_confirmation: "S&nha123")
-    assert funcionario.save
+    @funcionario = Funcionario.new(nome: "junior", email: "zezejunior@email.com", senha: "S&nha123", senha_confirmation: "S&nha123")
+    assert @funcionario.save
   end
 
   test "criar funcionario invalido" do
-    funcionario = Funcionario.new(nome: "", email: "", senha: "", senha_confirmation: "")
-    assert_not funcionario.save
+    @funcionario = Funcionario.new(nome: "", email: "", senha: "", senha_confirmation: "")
+    assert_not @funcionario.save
   end
 
   test "Atualizar funcionario" do
-    funcionario = Funcionario.new(nome: "junior", email: "zezejunior@email.com", senha: "S&nha123", senha_confirmation: "S&nha123")
-    assert funcionario.save
-    assert funcionario.update(name: "zeze junior")
+    @funcionario = Funcionario.new(nome: "junior", email: "zezejunior@email.com", senha: "S&nha123", senha_confirmation: "S&nha123")
+    assert @funcionario.save
+    assert @funcionario.update(name: "zeze junior")
   end
 end
